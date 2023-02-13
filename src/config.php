61fafe2980572d23b06a3a43682ed6f6ade97fb7
fengliyuan <vonly1024@gmail.com>
2023-02-13 14:50:13 +0800

    初始化1

--- src/config.php
+++ src/config.php
@@ -4,19 +4,29 @@ return [
         'scheme'   => 'tcp',
         'host'     => env('CACHE.host', '127.0.0.1'),
         'port'     => env('CACHE.port', '6379'),
-        'auth' => env('CACHE.password', ''),
-        'database'     => env('CACHE.select', 1),
+        'auth'     => env('CACHE.password', ''),
+        'database' => env('CACHE.select', 1),
     ],
     'ApiSign' => [
         'timeReduce' => 115, // 时间误差，如果超出误差，签名失效
     ],
-    'Upload'  => [
-        'sdk_ver'    => '7.6.0',
-        'block_size' => 4194304, //4*1024*1024 分块上传块大小，该参数为接口规格，不能修改
-        'up_host'    => 'http://attatch.c.jiaodong.cn/jd_attatch_serv/public/index.php/api/',
-    ],
-    'JwtS' => [],
+    'JwtS'    => [],
     'FastDfs' => [
         'baseUrl' => env('DFS.baseUrl', 'http://uploads.c.jiaodong.cn/'),     //服务器基地址
+    ],
+    'Aws'     => [
+        'BUCKET'                  => env('BUCKET', 'yanhuorongmei'),
+        'version'                 => env('BUCKET', 'latest'),
+        'region'                  => env('AWS_REGION', 'cn-north-1'),
+        'endpoint'                => env('AWS_ENDPOINT', 'http://192.168.10.7:9001'),
+        'use_path_style_endpoint' => env('use_path_style_endpoint', true),
+        'credentials'             => [
+            'key'    => env('AWS_KEY', 'BWYGJIHPd9E8Pvub'),
+            'secret' => env('AWS_SECRET', 'K8Kukjfa9JIG5mTUrqMIcNzOD6pDgUcE'),
+        ],
+        // You can override settings for specific services
+        'Ses'                     => [
+            'region' => env('AWS_SES_REGION', 'cn-north-1'),
+        ],
     ]
 ];
